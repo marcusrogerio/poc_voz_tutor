7ef59b984c7376baed1fab277c503177d3531d11
Marcus <marcusrogerio@gmail.com>
2025-11-18 20:55:19 -0300

    Adiciona gitignore para Python

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Marcus\projetos\aia\poc_voz_barge\backend\venv
+set VIRTUAL_ENV=C:\Users\Marcus\projetos\aia\poc_voz_tutor\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
